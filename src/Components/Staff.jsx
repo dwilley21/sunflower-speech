6637d50a000c7980111f7d8db5580809b4c258b7
dwilley21 <daltonwilley@gmail.com>
2020-02-10 23:40:05 -0600

    updated styles and added cards to staff page

--- src/Components/Staff.jsx
+++ src/Components/Staff.jsx
@@ -1,13 +1,57 @@
 import React, { Component } from 'react'
 import styled from 'styled-components';
+import Cards from '../Components/UI/Cards'
 
+const FlexIt = styled.div`
+ display: flex; 
+ flex-direction: row;
+ justify-content: center; 
+`
+const Header = styled.h1`
+text-align: center;
+`
 
 export default class Staff extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <>
+            <Header>Senior Staff</Header>
+            <FlexIt>
+                <Cards
+                name ="Nicholas Owen"
+                title="camp counselor"
+                description="Nick likes long walks on the beach"
+                />
+                <Cards
+                name ="Nicholas Owen"
+                title="camp counselor"
+                description="Nick likes long walks on the beach"
+                />
+                <Cards
+                name ="Nicholas Owen"
+                title="camp counselor"
+                description="Nick likes long walks on the beach"
+                />
+             </FlexIt>
+             <Header>Junior Staff</Header >
+            <FlexIt>
+                <Cards
+                name ="Nicholas Owen"
+                title="camp counselor"
+                description="Nick likes long walks on the beach"
+                />
+                <Cards
+                name ="Nicholas Owen"
+                title="camp counselor"
+                description="Nick likes long walks on the beach"
+                />
+                <Cards
+                name ="Nicholas Owen"
+                title="camp counselor"
+                description="Nick likes long walks on the beach"
+                />
+             </FlexIt>
+             </>
         )
     }
 }
--- src/Components/UI/Cards.jsx
+++ src/Components/UI/Cards.jsx
@@ -1,12 +1,48 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
-export default class Cards extends Component {
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
-    }
-}
+
+const Wrapper = styled.div` 
+  margin: 20px; 
+  padding: 35px;
+  border: 2px solid black; 
+`
+
+const Name = styled.h2`
+  color: black; 
+  font-weight: 300; 
+  border-bottom: 2px solid dodgerblue;
+  @media (max-wdth: 500px){
+    font-size: 1rem;
+  }
+`
+
+const Title = styled.div`
+  color: black; 
+  font-weight: 300; 
+  margin: 20px; 
+  @media (max-wdth: 500px){
+    font-size: .8rem;
+  }
+`
+const Description = styled.p`
+  color: black; 
+  font-weight: 300;
+  @media (max-wdth: 500px){
+    font-size: .75rem;
+  }
+
+`
+const Cards = ({
+  name, 
+  title, 
+  description
+}) => (
+  <Wrapper>
+    <Name>{name}</Name>
+    <Title>{title}</Title>
+    <Description>{description}</Description>
+  </Wrapper>
+)
+
+export default Cards;
